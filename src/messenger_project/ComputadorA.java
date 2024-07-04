9dddc50e4769731e4417a4c1403b3d876137511c
Felipe Figueiredo <felipe_figueiredo@outlook.com.br>
2024-07-04 09:50:57 -0300

    Conclusao do capitulo Encapsulamento

---
+++ src/messenger_project/ComputadorA.java
@@ -0,0 +1,12 @@
+package messenger_project;
+
+public class ComputadorA {
+    public static void main(String[] args) {
+
+        //Novo objeto
+        MSNMessenger msn = new MSNMessenger();
+
+        msn.enviarMensagem();
+        msn.receberMensagem();
+    }
+}
--- src/messenger_project/MSNMessenger.java
+++ src/messenger_project/MSNMessenger.java
@@ -2,15 +2,20 @@ package messenger_project;
 
 public class MSNMessenger {
     public void enviarMensagem(){
+
+        validarConectadoInternet(); //Metodo privado encapsulado
+
         System.out.println("Enviando mensagem");
+
+        salvarHistoricoMensagem(); // Metodo privado encapsulado
     }
     public void receberMensagem(){
         System.out.println("Recebendo mensagem");
     }
-    public void validarConectadoInternet(){
-        System.out.println("Validando se esta conectado a internet");
+    private void validarConectadoInternet(){
+        System.out.println("Validando conexao com a internet");
     }
-    public void salvarHistoricoMensagem(){
-        System.out.println("Salvando historico da mensagem");
+    private void salvarHistoricoMensagem(){
+        System.out.println("Salvando o historico da mensagem");
     }
 }
